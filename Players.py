52410dae15e8a3c967d76cc04799eb96d7d39ab2
Holowa21 <Holowalechnois@gmail.com>
2018-06-30 20:57:17 +0200

    debug

--- Players.py
+++ Players.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 
 lastTime = 0
 imageOrigin = [0, 0 ,32 ,32]
+numberAnimation = 0
 
 def canMove(lastTime):
     currentTime = time.time()
@@ -12,49 +13,57 @@ def animationRefresh(x, y, window, playersIMG, xP, yP):
     imageOrigin[0] = x
     imageOrigin[1] = y
     refresh(window, playersIMG, xP, yP, imageOrigin)
+
 def animationChange(x, window, playersIMG, xP, yP):
     pixelInY = [0, 32, 64]
     pixelInX = [0, 32, 64]
     if x == 0:
         for value in pixelInY:
             animationRefresh(0, value, window, playersIMG, xP, yP)
-            yP += 2
     elif x == 1:
         for value in pixelInX:
             animationRefresh(32, value, window, playersIMG, xP, yP)
-            xP += 2
     elif x == 2:
         for value in pixelInY:
             animationRefresh(64, value, window, playersIMG, xP, yP)
-            yP -= 2
     elif x == 3:
         for value in pixelInX:
             animationRefresh(96, value, window, playersIMG, xP, yP)
-            xP -= 2
-    return xP, yP
 
 def animationPlayers(keys, window, playersIMG, xP, yP):
     if keys == K_DOWN:
-        xP, yP = animationChange(0, window, playersIMG, xP, yP)
+        animationChange(0, window, playersIMG, xP, yP)
+        yP += 2
     elif keys == K_UP:
-        xP, yP = animationChange(2, window, playersIMG, xP, yP)
+        animationChange(2, window, playersIMG, xP, yP)
+        yP -= 2
     elif keys == K_RIGHT :
-        xP, yP = animationChange(1, window, playersIMG, xP, yP)
+        animationChange(1, window, playersIMG, xP, yP)
+        xP += 2
     elif keys == K_LEFT:
-        xP, yP = animationChange(3, window, playersIMG, xP, yP)
+        animationChange(3, window, playersIMG, xP, yP)
+        xP -= 2
     else:
-        refresh(window, playersIMG, xP, yP, imageOrigin)
-
+        animationRefresh(imageOrigin[0],0,window, playersIMG, xP, yP)
     return xP, yP
 
 def refresh(window, playerIMG, xP, yP, animation):
-    window.fill((255, 255, 255))
-    window.blit(playerIMG, (xP, yP), animation)
-    pygame.display.flip()
+    global numberAnimation
+    if numberAnimation == 21:
+        window.fill((255, 255, 255))
+        window.blit(playerIMG, (xP, yP), animation)
+        pygame.display.flip()
+        numberAnimation = 0
+    else:
+        numberAnimation += 1
+
+
+
+
 
 def move(keys,window, playersIMG, xP, yP ):
     global lastTime
-    if  canMove(lastTime):
+    if canMove(lastTime):
         xP, yP = animationPlayers(keys, window, playersIMG, xP, yP)
         lastTime = time.time()
     return xP, yP
